020f55fa8d51700114a6f36be92e65e159d550ef
reloadercf <elcfm1@gmail.com>
2020-06-13 21:43:12 -0500

    template ok

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require("axios")
 const parser = require('body-parser')
-const { users } = require('./endpoints')
+const { posts } = require('./endpoints')
 
 
 
@@ -11,15 +11,10 @@ app.use(parser.urlencoded({ extended: false }))
 app.use(parser.json())
 
 //inyeccion de dependencias
-const usersHandlers = users({ axios })
+const postsHandlers = users({ axios })
 
-app.get('/', usersHandlers.get);
+app.get('/', postsHandlers.get);
 
-app.post('/', usersHandlers.post);
-
-app.put('/:id', usersHandlers.put);
-
-app.delete('/:id', usersHandlers.delete);
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
