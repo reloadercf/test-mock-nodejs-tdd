fc9694ffa954c86dda6b10eda5f3d47e56ae2f27
reloadercf <elcfm1@gmail.com>
2020-06-13 21:23:34 -0500

    testing crud

--- endpoints/users/index.spec.js
+++ endpoints/users/index.spec.js
@@ -20,5 +20,74 @@ describe('Endpoints', () => {
                 ])
             })
         })
+        describe('post',()=>{
+            it('Creates a resource',async()=>{
+                let axios = {
+                    post: jest.fn().mockResolvedValue({ data: 1 }),
+                }
+                const res = {
+                    status: jest.fn().mockReturnThis(),
+                    send: jest.fn()
+                }
+                const req={
+                    body:'request body'
+                }
+                await handlers({ axios }).post(req, res)
+                expect(res.status.mock.calls).toEqual([
+                    [201]
+                ])
+                expect(res.send.mock.calls).toEqual([
+                    [1]
+                ])
+                expect(axios.post.mock.calls).toEqual([
+                    ['https://jsonplaceholder.typicode.com/users','request body']
+                ])
+            })
+        })
+        describe('put',()=>{
+            it('updates resources',async()=>{
+                let axios = {
+                    put: jest.fn().mockResolvedValue({ data: 1 }),
+                }
+                const req={
+                    body:'request body',
+                    params:{
+                        id:12,
+                    },
+                }
+                const res={
+                    sendStatus:jest.fn(),
+                }
+                await handlers({ axios }).put(req, res)
+                expect(axios.put.mock.calls).toEqual([
+                    ['https://jsonplaceholder.typicode.com/users/12','request body']
+                ])
+                expect(res.sendStatus.mock.calls).toEqual([
+                    [204]
+                ])
+            })
+        })
+        describe('delete',()=>{
+            it('delete a resource',async()=>{
+                let axios = {
+                    delete: jest.fn()
+                }
+                const req={
+                    params:{
+                        id:54,
+                    },
+                }
+                const res={
+                    sendStatus:jest.fn(),
+                }
+                await handlers({ axios }).delete(req, res)
+                expect(axios.delete.mock.calls).toEqual([
+                    ['https://jsonplaceholder.typicode.com/users/54']
+                ])
+                expect(res.sendStatus.mock.calls).toEqual([
+                    [204]
+                ])
+            })
+        })
     })
 })
